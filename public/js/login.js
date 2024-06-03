98316334b7ece4e868e6def2515b7cc9f4f8f112
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-06-02 23:19:57 -0500

    draft of header handlebars, very start of html js, need to finish views

---
+++ public/js/login.js
@@ -0,0 +1,8 @@
+
+
+
+document.getElementById('login').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    
+})
---
+++ public/js/logout.js
@@ -0,0 +1,8 @@
+
+
+
+document.getElementById('logout').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    
+})
