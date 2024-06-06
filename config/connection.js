68e09c5577e8690159d225892dbfc1ae001ef0e6
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-06-05 21:09:51 -0500

    soft coded user for deployment on Render

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // Postgres is hard coded as the username to reduce errors
 const sequelize = new Sequelize(
     process.env.DB_NAME,
-    'postgres',
+    process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
