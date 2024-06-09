68985da6138fecf49801c7ee3bd4b29e18df3dcd
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-06-08 19:52:30 -0500

    registration functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // Creates a new user
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
     try {
         const userData = await User.create(req.body);
         // Logs new user in after creation
---
+++ public/js/register.js
@@ -0,0 +1,28 @@
+//
+document.querySelector('.registration-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    const confirmPassword = document.querySelector('#confirmPassword').value.trim();
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match.');
+        return;
+    }
+
+    if (username && email && password) {
+        const response = await fetch('/api/users/register', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to register');
+        }
+    }
+});
